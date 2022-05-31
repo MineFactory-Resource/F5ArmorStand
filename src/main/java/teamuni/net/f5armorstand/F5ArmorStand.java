9716c2932b2e35d56cbc2e1151a2b8c62137d06e
Fade Jung <Fade@teamuni.net>
2022-05-31 18:55:17 +0900

    아머스탠드 플레이어 뒤에 띄우기
    비고:(아머스탠드 머리에 가스트 눈물을 씌움.)

--- src/main/java/teamuni/net/f5armorstand/F5ArmorStand.java
+++ src/main/java/teamuni/net/f5armorstand/F5ArmorStand.java
@@ -1,42 +1,44 @@
-package net.teamuni.f5armorstand;
+package teamuni.net.f5armorstand;
 
-
+import net.minecraft.world.entity.decoration.EntityArmorStand;
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
-
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Player;
-import org.bukkit.Location;
-import org.bukkit.event.player.PlayerMoveEvent;
 
-public final class F5ArmorStand extends JavaPlugin {
+import java.util.List;
+import java.util.ArrayList;
+
+public final class F5ArmorStand extends JavaPlugin implements Listener {
+    private List<EntityArmorStand> armorStands = new ArrayList<>();
 
     @Override
     public void onEnable() {
         Bukkit.getLogger().info("F5ArmorStand Enable. made by fade"); /* 플러그인 켜질때 실행되는 문구 */
-        ArmorStandView(); /* ArmorStandView 함수 호출 */
+        Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
+    @EventHandler
     public void ArmorStandView(PlayerMoveEvent e) {
-        if(Bukkit.getServer().getOnlinePlayers().size() != 0) {
             for (final Player p : Bukkit.getServer().getOnlinePlayers()) {
                 Location loc = p.getLocation();
                 ArmorStand as = p.getWorld().spawn(loc, ArmorStand.class);
                 as.setGravity(false);
                 as.setVisible(false);
                 as.setCustomNameVisible(false);
-                as.setHelmet(new ItemStack(Material.GHAST_TEAR));
-                if(p.isOnline()) {
-                    as.teleport(loc.add(0, 1, 0));
-                }
-                else {
-                    as.remove();
-                }
+                as.getEquipment().setHelmet(new ItemStack(Material.GHAST_TEAR));
+                as.teleport(loc.add(0, 1, 0));
+                as.remove();
             }
         }
-    }
+
+
 
     @Override
     public void onDisable() {
