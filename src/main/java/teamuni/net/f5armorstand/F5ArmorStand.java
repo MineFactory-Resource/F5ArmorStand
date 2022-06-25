91120a72017b6838651495354d951215084777d3
Fade Jung <Fade@teamuni.net>
2022-06-25 22:20:23 +0900

    특정 Permission이 있을경우 아머스탠드 Bypass

--- src/main/java/teamuni/net/f5armorstand/F5ArmorStand.java
+++ src/main/java/teamuni/net/f5armorstand/F5ArmorStand.java
@@ -1,7 +1,6 @@
 package teamuni.net.f5armorstand;
 
 
-
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.core.Vector3f;
 import net.minecraft.network.protocol.game.*;
@@ -32,58 +31,74 @@ public final class F5ArmorStand extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this); //이벤트 리스너 등록
-        Bukkit.getOnlinePlayers().forEach(player -> {
-            createArmorStand(player);
-            updateArmorStand(player);
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                if (!player.hasPermission("armorstand.noshow")) {
+                    createArmorStand(player);
+                    updateArmorStand(player);
+            }
         });
     }
-
     @Override
     public void onDisable() {
-        Bukkit.getOnlinePlayers().forEach(player -> { //서버 리로드시 아머스탠드가 남아있어 아머스탠드가 2개이상 생기는것을 방지하기위해 기존 아머스탠드 제거
-            armorStands.computeIfPresent(player.getUniqueId(), (k, v) -> {
-                ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityDestroy(v.ae()));
-                return null;
-            });
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            if (!player.hasPermission("armorstand.noshow")) {//서버 리로드시 아머스탠드가 남아있어 아머스탠드가 2개이상 생기는것을 방지하기위해 기존 아머스탠드 제거
+                armorStands.computeIfPresent(player.getUniqueId(), (k, v) -> {
+                    ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityDestroy(v.ae()));
+                    return null;
+                });
+            }
         });
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) { //플레이어 접속시 아머스탠드를 만들고 업데이트함.
-        createArmorStand(event.getPlayer());
-        updateArmorStand(event.getPlayer());
+    public void onJoin(PlayerJoinEvent event) {//플레이어 접속시 아머스탠드를 만들고 업데이트함.
+        Player player = event.getPlayer();
+        if (!player.hasPermission("armorstand.noshow")) {
+            createArmorStand(event.getPlayer());
+            updateArmorStand(event.getPlayer());
+        }
     }
 
-    @EventHandler
-    public void onChangeDimension(PlayerChangedWorldEvent event) { //차원(지옥문을 통해 네더 월드로 가거나, 엔드 차원문을 통해 엔드 월드를 가거나 등.) 이동시 아머스탠드를 만들고 업데이트를함.
-        createArmorStand(event.getPlayer());
-        updateArmorStand(event.getPlayer());
-    }
 
     @EventHandler
-    public void onMove(PlayerMoveEvent event) { //플레이어 이동시
-        updateArmorStand(event.getPlayer());
-    } //플레이어가 움직일때 아머스탠드 업데이트.
+    public void onChangeDimension(PlayerChangedWorldEvent event) {//차원(지옥문을 통해 네더 월드로 가거나, 엔드 차원문을 통해 엔드 월드를 가거나 등.) 이동시 아머스탠드를 만들고 업데이트를함.
+        Player player = event.getPlayer();
+        if (!player.hasPermission("armorstand.noshow")) {
+            createArmorStand(event.getPlayer());
+            updateArmorStand(event.getPlayer());
+        }
+    }
+
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        if (!player.hasPermission("armorstand.noshow")) { //플레이어 이동시
+            createArmorStand(event.getPlayer());
+            updateArmorStand(event.getPlayer()); //플레이어가 움직일때 아머스탠드 업데이트.
+        }
+    }
+
 
     private void createArmorStand(Player player) {
-        EntityArmorStand armorStand = new EntityArmorStand(EntityTypes.c, ((CraftWorld) player.getWorld()).getHandle()); //새로운 아머스탠드 생성
-        armorStand.j(true); //투명하게 설정
-        armorStand.t(true); //히트박스 제거
-        armorStands.put(player.getUniqueId(), armorStand);
-        ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutSpawnEntity(armorStand));
-        ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityEquipment(armorStand.ae(), Collections.singletonList(new Pair<>(EnumItemSlot.f, Items.pC.P_()))));
-        ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityMetadata(armorStand.ae(), armorStand.ai(), false));
-    }
+            EntityArmorStand armorStand = new EntityArmorStand(EntityTypes.c, ((CraftWorld) player.getWorld()).getHandle()); //새로운 아머스탠드 생성
+            armorStand.j(true); //투명하게 설정
+            armorStand.t(true); //히트박스 제거
+            armorStands.put(player.getUniqueId(), armorStand);
+            ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutSpawnEntity(armorStand));
+            ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityEquipment(armorStand.ae(), Collections.singletonList(new Pair<>(EnumItemSlot.f, Items.pC.P_()))));
+            ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityMetadata(armorStand.ae(), armorStand.ai(), false));
+        }
 
     private void updateArmorStand(Player player) {
-        EntityArmorStand armorStand = armorStands.get(player.getUniqueId());
-        if (armorStand == null) return;
-        Location loc = player.getLocation();
-        armorStand.a(loc.getX(), loc.getY(), loc.getZ());
-        armorStand.o(loc.getYaw());
-        armorStand.p(loc.getPitch());
-        armorStand.a(new Vector3f(player.getLocation().getPitch(), 0, 0));
-        ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityMetadata(armorStand.ae(), armorStand.ai(), false));
-        ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityTeleport(armorStand));
-    }
+            EntityArmorStand armorStand = armorStands.get(player.getUniqueId());
+            if (armorStand == null) return;
+            Location loc = player.getLocation();
+            armorStand.a(loc.getX(), loc.getY(), loc.getZ());
+            armorStand.o(loc.getYaw());
+            armorStand.p(loc.getPitch());
+            armorStand.a(new Vector3f(player.getLocation().getPitch(), 0, 0));
+            ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityMetadata(armorStand.ae(), armorStand.ai(), false));
+            ((CraftPlayer) player).getHandle().b.a(new PacketPlayOutEntityTeleport(armorStand));
+        }
 }
